ced24182387edcb47f4ae3aaee55c47dc96e51fc
Yuuta Sakai <ysakai.g@gmail.com>
2017-06-11 10:14:21 +0900

    Initial Commit

---
+++ PlainTextConv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlainTextConv
+//
+//  Created by 酒井雄太 on 2017/06/11.
+//  Copyright © 2017年 Yuuta Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
