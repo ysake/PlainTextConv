323bf7bc2f90997de517342bf7e140dd5e3ec8bb
Yuuta Sakai <ysakai.g@gmail.com>
2017-06-16 05:38:56 +0900

    mruby hello world

--- PlainTextConv/AppDelegate.swift
+++ PlainTextConv/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let mrb = mrb_open()
+        mrb_load_string(mrb, "puts 'mruby Hello, world!!'")
+        
         return true
     }
 
